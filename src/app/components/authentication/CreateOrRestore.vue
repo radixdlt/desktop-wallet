7bdc48bed22484413864e729a33668aa55451977
Alexander Wormbs <alexmedkex@gmail.com>
2020-06-02 17:21:16 +0100

    format

--- src/app/components/authentication/CreateOrRestore.vue
+++ src/app/components/authentication/CreateOrRestore.vue
@@ -23,57 +23,56 @@
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
-    import { radixApplication } from '@/app/modules/RadixApplication'
+import Vue from 'vue'
 
-    export default Vue.extend({
-        data() {
-            return {}
-        },
-        methods: {
-            createWallet() {
-                radixApplication.createWallet()
-            },
-            restoreWallet() {
-                radixApplication.restoreWallet()
-            },
-        },
-    })
+export default Vue.extend({
+  data() {
+    return {}
+  },
+  methods: {
+    createWallet() {
+      radixApplication.createWallet()
+    },
+    restoreWallet() {
+      radixApplication.restoreWallet()
+    },
+  },
+})
 </script>
 
 <style lang="scss" scoped>
-    .large-button {
-        margin-top: 16px;
-        cursor: pointer;
+.large-button {
+  margin-top: 16px;
+  cursor: pointer;
 
-        height: 100px;
-        border-radius: 3px;
-        font-size: 14px;
+  height: 100px;
+  border-radius: 3px;
+  font-size: 14px;
 
-        background-color: $grey-light;
+  background-color: $grey-light;
 
-        padding: 32px 20px;
+  padding: 32px 20px;
 
-        &:hover {
-            background-color: $grey-light;
-            box-shadow: 0 2px 4px 0 rgba(163, 173, 183, 0.04), 0 12px 30px 0 rgba(82, 95, 127, 0.12);
-        }
+  &:hover {
+    background-color: $grey-light;
+    box-shadow: 0 2px 4px 0 rgba(163, 173, 183, 0.04),
+      0 12px 30px 0 rgba(82, 95, 127, 0.12);
+  }
 
-        .icon {
-            float: left;
-            width: 36px;
-            height: 36px;
-            object-fit: contain;
-            border-radius: 9px;
-            background-color: rgba(#dbdde0, .3);
-            color: $green;
+  .icon {
+    float: left;
+    width: 36px;
+    height: 36px;
+    object-fit: contain;
+    border-radius: 9px;
+    background-color: rgba(#dbdde0, 0.3);
+    color: $green;
+  }
 
-        }
-
-        .text {
-            display: inline-block;
-            margin-left: 10px;
-            line-height: 36px;
-        }
-    }
+  .text {
+    display: inline-block;
+    margin-left: 10px;
+    line-height: 36px;
+  }
+}
 </style>
