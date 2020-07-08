5cab1b23c2fa2efec42e0cc9de459d0601e7c1eb
Alex Wormbs <alexander.wormbs@gmail.com>
2020-07-08 10:12:13 +0100

    add atom hash confirm

--- src/app/components/hardware-wallet/ConfirmSign.vue
+++ src/app/components/hardware-wallet/ConfirmSign.vue
@@ -4,16 +4,22 @@ div
       div.panel
         div.form.body.modal-panel
             div.title Confirm sign
-            div(v-if="ledgerAppOpen").
-              Waiting for confirmation on your hardware wallet device.
+            div(v-if="ledgerAppOpen")
+              p.
+                Waiting for confirmation on your hardware wallet device.
+              p.atomHash.
+                Signing atom hash: {{ atomHash }}
             div(v-else).
               Please open the Radix Ledger App on your device.
+              
                   
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Modal from '../shared/Modal.vue'
+import { atomHashObservable } from '@radixdlt/hardware-wallet'
+
 export default Vue.extend({
   components: {
     Modal,
@@ -24,13 +30,26 @@ export default Vue.extend({
       required: true,
     },
   },
+  data() {
+    return {
+      atomHash: '',
+    }
+  },
   computed: {
     ledgerAppOpen(): boolean {
       return this.$store.state.ledgerAppOpen
     },
   },
+  created() {
+    atomHashObservable.subscribe(hash => {
+      this.atomHash = hash
+    })
+  },
 })
 </script>
 
 <style lang="scss" scoped>
+.atomHash {
+  margin-top: 10px;
+}
 </style>
